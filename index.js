4629c644eb551032cce8b6d336cafdf2e629da59
Ihor Rudniev <rudnievihor@gmail.com>
2021-08-26 22:42:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Задание - 1
+// Задание - 1 ================================================
 
 const delay = (ms) => {
   // Твой код
-  new Promise((resolved) => {
+  return new Promise((resolved) => {
     setTimeout(() => {
       resolved(ms);
     }, ms);
@@ -15,3 +15,34 @@ const logger = (time) => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+// Задание - 2 =================================================
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName, callback) => {
+  const updatedUsers = allUsers.map((user) =>
+    user.name === userName ? { ...user, active: !user.active } : user
+  );
+
+  callback(updatedUsers);
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+toggleUserState(users, "Mango", logger);
+toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
